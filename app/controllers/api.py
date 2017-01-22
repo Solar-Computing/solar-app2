16a0529b44acf3180c3bc08a024122ee5b40b1d9
Dhruv Mehra <dmehra6@gatech.edu>
2017-01-22 12:23:59 -0500

    Set up test route with Tommy's settings data

--- app/controllers/api.py
+++ app/controllers/api.py
@@ -17,3 +17,41 @@ def getDaily():
 @api.route('/weeklyLoad')
 def getWeekly():
     return jsonify(load='week')
+
+@api.route('/test')
+def test():
+    return jsonify(
+        [
+            {"name": "General", "toggle": {"switchIsOn": False}, "options": [
+                {"name": "Temperature", "optionType": "slider", "state": {"minimum": "20", "maximum":"120"}},
+                {"name": "Microwave", "optionType": "switch", "state": {"switchIsOn": False}},
+                {"name": "Dishwasher", "optionType": "switch", "state": {"switchIsOn": False}}
+                ]
+            },
+            {"name": "Kitchen", "toggle": {"switchIsOn": False}, "options": [
+                {"name": "Lights", "optionType": "switch", "state": {"switchIsOn": False}},
+                {"name": "Microwave", "optionType": "switch", "state": {"switchIsOn": False}},
+                {"name": "Dishwasher", "optionType": "switch", "state": {"switchIsOn": False}}
+                ]
+            },
+            {"name": "Living Room", "toggle": {"switchIsOn": False}, "options": [
+                {"name": "Light 1", "optionType": "switch", "state": {"switchIsOn": False}},
+                {"name": "Light 2", "optionType": "switch", "state": {"switchIsOn": False}},
+                {"name": "Outlet 1", "optionType": "switch", "state": {"switchIsOn": False}},
+                {"name": "Outlet 2", "optionType": "switch", "state": {"switchIsOn": False}}
+                ]
+            },
+            {"name": "Bed Room", "toggle": {"switchIsOn": False}, "options": [
+                {"name": "Outlet 1", "optionType": "switch", "state": {"switchIsOn": False}},
+                {"name": "Outlet 2", "optionType": "switch", "state": {"switchIsOn": False}},
+                {"name": "Heater", "optionType": "switch", "state": {"switchIsOn": False}},
+                {"name": "Lights", "optionType": "switch", "state": {"switchIsOn": False}},
+                {"name": "Lights", "optionType": "switch", "state": {"switchIsOn": False}},
+                {"name": "Lights", "optionType": "switch", "state": {"switchIsOn": False}},
+                {"name": "Lights", "optionType": "switch", "state": {"switchIsOn": False}},
+                {"name": "Lights", "optionType": "switch", "state": {"switchIsOn": False}},
+                {"name": "Lights", "optionType": "switch", "state": {"switchIsOn": False}},
+                {"name": "Lights", "optionType": "switch", "state": {"switchIsOn": False}}
+                ]
+            }
+        ])
