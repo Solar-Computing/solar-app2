4f2145982e8133ec361b5c324a09b709bf05b9d6
Andrew Dai <a@andrewdai.co>
2017-01-29 13:39:51 -0500

    fix?

--- app/controllers/api.py
+++ app/controllers/api.py
@@ -11,6 +11,7 @@ def getSimulation():
     body = request.get_json()
     start = body['start']
     end = body['end']
+    aggregate = body['aggregate'] or 'hourly'
     results = db.session.execute(Simulation.getRange(start, end)).fetchall()
     return jsonify(contents=results, average="not implemented")
 
