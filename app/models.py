de0896d2e612beb16fe36d7b8a4e6eecec8bcc9a
Dhruv Mehra <dmehra6@gatech.edu>
2017-04-04 09:51:38 -0400

    Add the simulation data models

--- app/models.py
+++ app/models.py
@@ -3,16 +3,19 @@ from sqlalchemy import func
 
 db = SQLAlchemy()
 
+
 class Unit(db.Model):
     table = db.Column(db. String(), primary_key=True)
     field = db.Column(db.String(), primary_key=True)
     unit = db.Column(db.String())
 
+
 class Simulation(db.Model):
-    timestamp = db.Column(db.DateTime(timezone=False), primary_key=True) # datetime object
+    timestamp = db.Column(db.DateTime(timezone=False),
+                          primary_key=True)  # datetime object
     globalSolar = db.Column(db.String())
-    PVPowerOutput = db.Column(db.Float()) # solar power produced kw
-    ACPrimaryLoad = db.Column(db.Float()) # total power consumed kw
+    PVPowerOutput = db.Column(db.Float())  # solar power produced kw
+    ACPrimaryLoad = db.Column(db.Float())  # total power consumed kw
     ACPrimaryLoadServed = db.Column(db.Float())
     gridPowerPrice = db.Column(db.Float())
     gridSellbackRate = db.Column(db.Float())
@@ -32,12 +35,11 @@ class Simulation(db.Model):
 
     def getRange(start, end, aggregate):
         query = db.select([
-                func.max(Simulation.timestamp).label('timestamp'),
-                func.sum(Simulation.PVPowerOutput).label('PVPowerOutput'),
-                func.sum(Simulation.ACPrimaryLoad).label('ACPrimaryLoad')\
-            ])\
+            func.max(Simulation.timestamp).label('timestamp'),
+            func.sum(Simulation.PVPowerOutput).label('PVPowerOutput'),
+            func.sum(Simulation.ACPrimaryLoad).label('ACPrimaryLoad')])
             .where(Simulation.timestamp >= start)\
-            .where(Simulation.timestamp <= end)\
+                .where(Simulation.timestamp <= end)\
 
         if aggregate == 'hourly':
             # do nothing
@@ -47,23 +49,24 @@ class Simulation(db.Model):
             query = query.group_by(func.date(Simulation.timestamp))
         elif aggregate == 'monthly':
             # group by month
-            query = query.group_by(func.date_part('month', Simulation.timestamp))
+            query = query.group_by(func.date_part(
+                'month', Simulation.timestamp))
         else:
             raise ValueError('invalid aggregation')
         return [dict(r) for r in db.session.execute(query).fetchall()]
 
     def getAverage(start, end):
         query = db.select([
-                func.avg(Simulation.PVPowerOutput).label('PVPowerOutputAverage'),
-                func.avg(Simulation.ACPrimaryLoad).label('ACPrimaryLoadAverage')\
-            ])\
-            .where(Simulation.timestamp >= start)\
+            func.avg(Simulation.PVPowerOutput).label('PVPowerOutputAverage'),
+            func.avg(Simulation.ACPrimaryLoad).label('ACPrimaryLoadAverage')
+        ]).where(Simulation.timestamp >= start)\
             .where(Simulation.timestamp <= end)
         return dict(db.session.execute(query).fetchone())
 
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
+
 
 class Home(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -79,6 +82,7 @@ class Home(db.Model):
             'id': self.id,
             'name': self.name,
         }
+
 
 class Circuit(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -97,9 +101,12 @@ class Circuit(db.Model):
             'homeId': self.homeId,
         }
 
+
 class PowerConsumption(db.Model):
-    timestamp = db.Column(db.DateTime(timezone=False), primary_key=True) # datetime object
-    circuitId = db.Column(db.Integer, db.ForeignKey('circuit.id'), nullable=False, primary_key=True)
+    timestamp = db.Column(db.DateTime(timezone=False),
+                          primary_key=True)  # datetime object
+    circuitId = db.Column(db.Integer, db.ForeignKey(
+        'circuit.id'), nullable=False, primary_key=True)
     power = db.Column(db.Float(), nullable=False)
 
     def serialize(self):
@@ -107,4 +114,32 @@ class PowerConsumption(db.Model):
             'timestamp': self.timestamp,
             'circuitId': self.circuitId,
             'power': self.power,
+        }
+
+
+class QuestSimulation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    datetime = db.Column(db.DateTime(timezone=False))      # datetime obj
+    apartmentNo = db.Column(db.Integer, nullable=False)
+    load = db.Column(db.Float)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'datetime': self.datetime,
+            'apartmentNo': self.apartmentNo,
+            'load': self.load
+        }
+
+
+class UnitCost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    datetime = db.Column(db.DateTime(timezone=False))
+    cost = db.Column(db.Float)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'datetime': self.datetime,
+            'cost': self.cost
         }
